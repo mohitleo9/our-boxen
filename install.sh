4fd4588126d1b6cb3f0cd03ab2ed8c922a4715da
maggarwal <maggarwal@amplify.com>
2015-03-27 12:46:53 -0400

    don't be too lazy

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ sudo mkdir -p /opt/boxen
 sudo chown ${USER}:staff /opt/boxen
 git clone https://github.com/mohitleo9/our-boxen /opt/boxen/repo
 cd /opt/boxen/repo
-./script/boxen --no-fde
+# ./script/boxen --no-fde
